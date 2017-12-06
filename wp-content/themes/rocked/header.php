ca90066b4ce8b973703c5ce4997307b57c194060
epereyra <emmanuel.pereyra@mercadolibre.com>
2017-12-06 10:15:09 -0300

    ajuste de header

--- wp-content/themes/rocked/header.php
+++ wp-content/themes/rocked/header.php
@@ -36,6 +36,13 @@
 					</div><!-- /.col-md-2 -->
 					<div class="menu-wrapper col-md-9 col-sm-9 col-xs-9">
 						<div class="btn-menu"><i class="fa fa-bars"></i></div>
+						<ul class="pre-mainnav">
+							<li>call us:  +34 91 658 4245</li>
+							<li>|</li>
+							<li>language: eng <i class="fa fa-angle-down"></i></li>
+							<li>|</li>
+							<li><i class="fa fa-search"></i></li>
+						</ul>
 						<nav id="mainnav" class="mainnav">
 							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 						</nav><!-- #site-navigation -->
